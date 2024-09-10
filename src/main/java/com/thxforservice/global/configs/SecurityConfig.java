a6e6affdd831e2406fe866abc6c05d75345b8a3a
irine3355 <irine3355@gmail.com>
2024-09-10 17:27:51 +0900

    시큐리티 수정

--- src/main/java/com/thxforservice/global/configs/SecurityConfig.java
+++ src/main/java/com/thxforservice/global/configs/SecurityConfig.java
@@ -36,7 +36,10 @@ public class SecurityConfig {
                    h.accessDeniedHandler((req, res, e) -> res.sendError(HttpStatus.UNAUTHORIZED.value()));
                 })
                 .authorizeHttpRequests(c -> {
-                        c.anyRequest().permitAll();
+                     /*   c.anyRequest().permitAll(); */
+                    c.requestMatchers("/admin/**").hasAnyAuthority("ADMIN")
+                            .requestMatchers("/apidocs.html", "/swagger-ui/**").permitAll()
+                            .anyRequest().authenticated();
                 });
 
         return http.build();
