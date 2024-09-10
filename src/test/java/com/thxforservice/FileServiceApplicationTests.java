d0981f6328e59ded0e9a84ad739933b1ee551c35
irine3355 <irine3355@gmail.com>
2024-09-10 18:13:21 +0900

    시큐리티 수정

---
+++ src/test/java/com/thxforservice/FileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.choongang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
